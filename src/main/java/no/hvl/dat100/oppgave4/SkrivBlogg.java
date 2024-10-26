7977698f6e23f80dd92c4349a0ae7e946f49d280
h184909 <184909@stud.hvl.no>
2024-10-26 14:09:27 +0200

    hfdgfd

--- src/main/java/no/hvl/dat100/oppgave4/SkrivBlogg.java
+++ src/main/java/no/hvl/dat100/oppgave4/SkrivBlogg.java
@@ -2,14 +2,30 @@ package no.hvl.dat100.oppgave4;
 
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 import no.hvl.dat100.common.TODO;
 import no.hvl.dat100.oppgave3.*;
+
+
 
 public class SkrivBlogg {
 
 	public static boolean skriv(Blogg samling, String mappe, String filnavn) {
 
-		throw new UnsupportedOperationException(TODO.method());
+		String filSti = mappe + "/" + filnavn;
+		
+		try (PrintWriter writer = new PrintWriter(new FileWriter(filSti))){
+			writer.print(samling.toString());
+			return true;
+		} catch (IOException e) {
+			System.out.println("Feil Oppstod ved skriving til fil: " + e.getMessage());
+			return false;
+		}
+		
+		
+		
 	}
 }
